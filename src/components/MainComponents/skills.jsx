2cd8b77121fc028ea52d37f0fbda59632159b0c9
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-02-03 20:36:18 -0300

    Update Add Github Msg Error Api

--- src/components/MainComponents/skills.jsx
+++ src/components/MainComponents/skills.jsx
@@ -5,21 +5,16 @@ export default function skills() {
         <section id="skills" className="second-section">
             <h1 data-aos="flip-up" data-aos-duration="1500">Tech Stack</h1>
             <div className="skills-container">
-                <img src="https://skillicons.dev/icons?i=html&theme=dark&size=32" alt="HTML" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=css&theme=dark&size=32" alt="CSS" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=js&theme=dark&size=32" alt="JavaScript" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=react&theme=dark&size=32" alt="React" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=git&theme=dark&size=32" alt="Git" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=github&theme=dark&size=32" alt="GitHub" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=cpp&theme=dark&size=32" alt="C++" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" alt="HTML" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" alt="CSS" data-aos="fade-down" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" alt="JavaScript" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" alt="React" data-aos="fade-down" data-aos-duration="1500" />
                 <img src={PawnIcon} alt="Pawn" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=vscode&theme=dark&size=32" alt="VSCode" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=discord&theme=dark&size=32" alt="Discord" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=nodejs&theme=dark&size=32" alt="Node.js" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=py&theme=dark&size=32" alt="Python" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=java&theme=dark&size=32" alt="Java" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=figma&theme=dark&size=32" alt="Figma" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=mysql&theme=dark&size=32" alt="MySQL" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg" alt="Node.js" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" alt="Java" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" alt="Git" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" alt="GitHub" data-aos="fade-down" data-aos-duration="1500" />
+                <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" alt="VSCode" data-aos="fade-up" data-aos-duration="1500" />
             </div>
         </section>
     );
