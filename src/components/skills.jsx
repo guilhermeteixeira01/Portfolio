5a4b77c949fd88aae6b8a6234b3b2f88f8e82bda
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-02-03 16:29:19 -0300

    Fix Animations

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -12,14 +12,14 @@ export default function skills() {
                 <img src="https://skillicons.dev/icons?i=git&theme=dark&size=32" alt="Git" data-aos="fade-up" data-aos-duration="1500" />
                 <img src="https://skillicons.dev/icons?i=github&theme=dark&size=32" alt="GitHub" data-aos="fade-down" data-aos-duration="1500" />
                 <img src="https://skillicons.dev/icons?i=cpp&theme=dark&size=32" alt="C++" data-aos="fade-up" data-aos-duration="1500" />
-                <img src={PawnIcon} alt="Pawn" />
-                <img src="https://skillicons.dev/icons?i=discord&theme=dark&size=32" alt="Discord" data-aos="fade-down" data-aos-duration="1500" />
+                <img src={PawnIcon} alt="Pawn" data-aos="fade-down" data-aos-duration="1500" />
                 <img src="https://skillicons.dev/icons?i=vscode&theme=dark&size=32" alt="VSCode" data-aos="fade-up" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=nodejs&theme=dark&size=32" alt="Node.js" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=java&theme=dark&size=32" alt="Java" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://skillicons.dev/icons?i=discord&theme=dark&size=32" alt="Discord" data-aos="fade-down" data-aos-duration="1500" />
+                <img src="https://skillicons.dev/icons?i=nodejs&theme=dark&size=32" alt="Node.js" data-aos="fade-up" data-aos-duration="1500" />
                 <img src="https://skillicons.dev/icons?i=py&theme=dark&size=32" alt="Python" data-aos="fade-down" data-aos-duration="1500" />
-                <img src="https://skillicons.dev/icons?i=mysql&theme=dark&size=32" alt="MySQL" data-aos="fade-up" data-aos-duration="1500" />
+                <img src="https://skillicons.dev/icons?i=java&theme=dark&size=32" alt="Java" data-aos="fade-up" data-aos-duration="1500" />
                 <img src="https://skillicons.dev/icons?i=figma&theme=dark&size=32" alt="Figma" data-aos="fade-down" data-aos-duration="1500" />
+                <img src="https://skillicons.dev/icons?i=mysql&theme=dark&size=32" alt="MySQL" data-aos="fade-up" data-aos-duration="1500" />
             </div>
         </section>
     );
